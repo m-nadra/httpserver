3d7670501a5539f97affcaa00fb52ebdeb3ca3ef
Mikolaj Nadra <m.nadra@proton.me>
2026-05-21 10:41:34 +0200

    create Route and Router structs

--- src/routes.rs
+++ src/routes.rs
@@ -1,22 +1,54 @@
-use std::collections::HashMap;
-
 use crate::{HttpRequest, HttpResponse};
 
-fn default(_: &HttpRequest, response: &mut HttpResponse) {
-    response.send_html("<h1>Default route</h1>".to_owned());
+type FunctionHandler = fn(&HttpRequest, &mut HttpResponse);
+
+struct Route {
+    path: String,
+    method: String,
+    function: FunctionHandler,
 }
 
-fn json(_: &HttpRequest, response: &mut HttpResponse) {
-    response.send_json("{\"message\": \"Hello World!\"}".to_owned());
+struct Router {
+    routes: Vec<Route>,
 }
-pub fn router(request: &HttpRequest, response: &mut HttpResponse) {
-    let mut routes: HashMap<String, fn(&HttpRequest, &mut HttpResponse)> = HashMap::new();
-    routes.insert("/".to_owned(), default);
-    routes.insert("/json".to_owned(), json);
 
-    if let Some(func) = routes.get(&request.path) {
-        func(request, response);
-    } else {
-        response.status = 404;
+impl Router {
+    pub fn default() -> Self {
+        Self { routes: Vec::new() }
     }
+    pub fn get(&mut self, path: String, function: FunctionHandler) {
+        self.routes.push(Route {
+            method: "GET".to_owned(),
+            path,
+            function,
+        });
+    }
+    pub fn find_endpoint(&self, path: &String, method: &String) -> Result<FunctionHandler, u16> {
+        let mut status = 404;
+
+        for route in &self.routes {
+            if route.path == *path {
+                status = 405;
+                if route.method == *method {
+                    return Ok(route.function);
+                }
+            }
+        }
+        Err(status)
+    }
+}
+
+pub fn router(request: &HttpRequest, response: &mut HttpResponse) {
+    let mut router = Router::default();
+    router.get("/".to_owned(), |_, res| {
+        res.send_html("<h1>Default route</h1>".to_owned());
+    });
+    router.get("/json".to_owned(), |_, res| {
+        res.send_json("{\"message\": \"Hello World!\"}".to_owned());
+    });
+
+    match router.find_endpoint(&request.path, &request.method) {
+        Ok(func) => func(request, response),
+        Err(code) => response.status = code,
+    };
 }
